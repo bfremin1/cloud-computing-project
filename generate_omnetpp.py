62f22b394114fca628bfbe7c704b085c640f67c8
bfremin1 <bfremin1@jhu.edu>
2022-04-10 19:20:19 -0400

    Starting to add routers

--- generate_omnetpp.py
+++ generate_omnetpp.py
@@ -1,26 +1,52 @@
 import math, random
 import numpy as np
 
-# Change WORKING_DIRECTORY and PACKAGE to run on local device
 WORKING_DIRECTORY = "C:/Users/bfrem/Documents/CloudComputing/omnetpp-5.7-windows-x86_64/omnetpp-5.7/samples/inet4/examples/inet/DatacenterTopologies"
+
 PACKAGE = "inet.examples.inet.DatacenterTopologies"
 
 IMPORTS = [
     "inet.networklayer.configurator.ipv4.Ipv4NetworkConfigurator",
     "inet.node.inet.Router",
     "inet.node.inet.StandardHost",
-    "ned.DatarateChannel"
+    "ned.DatarateChannel",
+    "inet.visualizer.integrated.IntegratedVisualizer"
 ]
 
 DEFAULT_CHANNEL = "defaultline"
 DEFAULT_LINE_PARAMS = {
-    "delay": "0.1us",
-    "datarate": "1Gbps",
+    "delay": "1us",
+    "datarate": "8Mbps",
+    "per": "0"
+}
+
+INFINITE_CHANNEL = "infiniteline"
+INFINITE_LINE_PARAMS = {
+    "delay": "0s",
+    "datarate": "1000Gbps",
     "per": "0"
 }
 
 TAB = "    "
 
+SIZE_SMALL = "s"
+SIZE_VERY_SMALL = "vs"
+
+ICON_ROUTER = "abstract/router"
+ICON_SOURCE = "block/source"
+ICON_SINK = "block/sink"
+ICON_PC = "device/pc"
+ICON_LAPTOP = "device/laptop"
+ICON_SERVER = "device/server"
+ICON_CONFIGURATOR = "block/cogwheel"
+ICON_VISUALIZER = "block/app"
+
+NED_CONFIGURATOR = "Ipv4NetworkConfigurator"
+NED_VISUALIZER = "IntegratedVisualizer"
+NED_ROUTER = "Router"
+NED_STANDARD_HOST = "StandardHost"
+
+"""
 ROUTER_NED = "Router"
 ROUTER_TYPE = "Router"
 ROUTER_SIZE = "s"
@@ -32,6 +58,7 @@ SERVER_TYPE = "Server"
 STANDARD_HOST_SIZE = "s"
 CLIENT_ICON = "device/laptop"
 SERVER_ICON = "device/server"
+"""
 
 SERVER_PORT = 1100
 REQUEST_SIZE = 1300
@@ -73,7 +100,6 @@ class Flow:
 
         return ini_str
 
-
 class Line:
     def __init__(self, channel=DEFAULT_CHANNEL, params=DEFAULT_LINE_PARAMS):
         self._channel = channel
@@ -96,7 +122,7 @@ class Configurator:
         self._x = int(x)
         self._y = int(y)
         self._size = "s"
-        self._icon = "block/cogwheel"
+        self._icon = ICON_CONFIGURATOR
     
     def ned_submodule(self):
         return (
@@ -106,22 +132,31 @@ class Configurator:
             " }"
         )
 
-class Node:
-    def __init__(self, name, node_type, x, y):
-        assert node_type in [ROUTER_TYPE, CLIENT_TYPE, SERVER_TYPE]
-        self._type = node_type
+class Visualizer:
+    def __init__(self, x, y, name="visualizer"):
         self._name = name
-        self._ned = ROUTER_NED if node_type == ROUTER_TYPE else STANDARD_HOST_NED
-        self._icon = ROUTER_ICON
-        if node_type == CLIENT_TYPE:
-            self._icon = CLIENT_ICON
-        elif node_type == SERVER_TYPE:
-            self._icon = SERVER_ICON
+        self._x = int(x)
+        self._y = int(y)
+        self._size = "s"
+        self._icon = ICON_VISUALIZER
+    
+    def ned_submodule(self):
+        return (
+            f"{self._name}: IntegratedVisualizer" +
+            " { parameters:" +
+            f" @display(\"p={self._x},{self._y};i={self._icon};is={self._size}\");" +
+            " }"
+        )
+
+class Node:
+    def __init__(self, name, x, y):
+        self._name = name
         self._size = "s"
         self._x = int(x)
         self._y = int(y)
         self._links = []
         self._flows = []
+        self._ip = None
     
     @staticmethod
     def encode_link(link):
@@ -168,7 +203,7 @@ class Node:
     def get_name(self):
         return self._name
     
-    def get_type(self):
+    def get_ned(self):
         return self._type
     
     def get_num_flows(self):
@@ -205,8 +240,53 @@ class Node:
             " }"
         )
 
+class Router(Node):
+    def __init__(self, name, x, y):
+        super().__init__(name, x, y)
+        self._ned = NED_ROUTER
+        self._icon = ICON_ROUTER
+        self._size = SIZE_SMALL
+
+class Source(Node):
+    def __init__(self, name, x, y):
+        super().__init__(name, x, y)
+        self._ned = NED_STANDARD_HOST
+        self._icon = ICON_SOURCE
+        self._size = SIZE_VERY_SMALL
+
+class Sink(Node):
+    def __init__(self, name, x, y):
+        super().__init__(name, x, y)
+        self._ned = NED_STANDARD_HOST
+        self._icon = ICON_SINK
+        self._size = SIZE_VERY_SMALL
+
+class Server(Node):
+    def __init__(self, name, x, y):
+        super().__init__(name, x, y)
+        self._ned = NED_ROUTER
+        self._icon = ICON_SERVER
+        self._size = SIZE_SMALL
+
+"""
+    assert node_type in [ROUTER_TYPE, CLIENT_TYPE, SERVER_TYPE]
+    self._type = node_type
+    self._name = name
+    self._ned = ROUTER_NED if node_type == ROUTER_TYPE else STANDARD_HOST_NED
+    self._icon = ROUTER_ICON
+    if node_type == CLIENT_TYPE:
+        self._icon = CLIENT_ICON
+    elif node_type == SERVER_TYPE:
+        self._icon = SERVER_ICON
+    self._size = "s"
+    self._x = int(x)
+    self._y = int(y)
+    self._links = []
+    self._flows = []
+"""
+    
 class Network:
-    def __init__(self, name, package=PACKAGE, imports=IMPORTS, width=400, height=300):
+    def __init__(self, name, package=PACKAGE, imports=IMPORTS, width=800, height=500, use_visualizer=False):
         self._name = name
         self._package = package
         self._imports = imports
@@ -214,26 +294,58 @@ class Network:
         self._height = int(height)
         self._node_map = {}
         self._configurator = Configurator(50, 50)
+        self._visualizer = Visualizer(50, 100) if use_visualizer else None
+        self._source_sink_counter = 0
         self._lines = []
         self._flows = []
     
     def get_name(self):
         return self._name
+
+    def add_node(self, node):
+        if node.get_name() in self._node_map.keys():
+            print(f"ERROR: {node.get_name()} is already in map keys: {list(self._node_map.keys())}")
+            return
+        self._node_map[node.get_name()] = node
     
     def initialize(self):
-        self._lines = [Line()]
-        channel = self._lines[0].get_channel()
+        self._lines = [
+            Line(DEFAULT_CHANNEL, DEFAULT_LINE_PARAMS), 
+            Line(INFINITE_CHANNEL, INFINITE_LINE_PARAMS)
+        ]
         self._node_map = {}
-        self._node_map["router"] = Node("router", ROUTER_TYPE, 0.5 * self._width, self._height / 2)
-        self._node_map["client"] = Node("client", CLIENT_TYPE, 0.25 * self._width, self._height / 2)
-        self._node_map["server"] = Node("server", SERVER_TYPE, 0.75 * self._width, self._height / 2)
+        self.add_node(Server("server0", 1 / 4 * self._width, self._height / 2))
+        self.add_node(Server("server1", 3 / 4 * self._width, self._height / 2))
+        self.add_node(Router("router0", 2 / 4 * self._width, self._height * 1 / 4))
+        self.add_node(Router("router1", 2 / 4 * self._width, self._height * 2 / 4))
+        self.add_node(Router("router2", 2 / 4 * self._width, self._height * 3 / 4))
 
-        Node.connect(self._node_map["client"], self._node_map["router"], channel)
-        Node.connect(self._node_map["server"], self._node_map["router"], channel)
+        Node.connect(self._node_map["server0"], self._node_map["router0"], DEFAULT_CHANNEL)
+        Node.connect(self._node_map["server0"], self._node_map["router1"], DEFAULT_CHANNEL)
+        Node.connect(self._node_map["server0"], self._node_map["router2"], DEFAULT_CHANNEL)
+        Node.connect(self._node_map["server1"], self._node_map["router0"], DEFAULT_CHANNEL)
+        Node.connect(self._node_map["server1"], self._node_map["router1"], DEFAULT_CHANNEL)
+        Node.connect(self._node_map["server1"], self._node_map["router2"], DEFAULT_CHANNEL)
 
-        self._node_map["client"].add_flow(self._node_map["server"], 1000000, 0)
+        # self._node_map["client"].add_flow(self._node_map["server"], 1000000, 0)
+
+        self.add_flow(self._node_map["server0"], self._node_map["server1"], 1000000, 0)
+        self.add_flow(self._node_map["server0"], self._node_map["server1"], 1000000, 0)
+        self.add_flow(self._node_map["server0"], self._node_map["server1"], 1000000, 0)
 
         return self
+
+    def add_flow(self, send, recv, amount, start):
+        source_name = f"source{self._source_sink_counter}"
+        sink_name = f"sink{self._source_sink_counter}"
+        self._source_sink_counter += 1
+        source = Source(source_name, send._x - 25, send._y + 50)
+        Node.connect(source, send, INFINITE_CHANNEL)
+        sink = Sink(sink_name, recv._x + 25, recv._y + 50)
+        Node.connect(sink, recv, INFINITE_CHANNEL)
+        source.add_flow(sink, amount, start)
+        self.add_node(source)
+        self.add_node(sink)
 
     def ned_connections(self):
         # alphabetize links :)
@@ -248,9 +360,25 @@ class Network:
 
     def ned_submodules(self):
         submodules = set([self._configurator.ned_submodule()])
+        if self._visualizer:
+            submodules.add(self._visualizer.ned_submodule())
         for node in self._node_map.values():
             submodules.add(node.ned_submodule())
         return sorted(list(submodules))
+
+    def xml_interfaces(self):
+        interfaces = []
+        ip_major = 1
+        ip_minor = 0
+        for node in sorted(self._node_map.values(), key=lambda n: n.get_name()):
+            for i in range(node.get_num_gates()):
+                interfaces.append(f"<interface hosts='{node.get_name()}' names='ppp{i}' address='{ip_major}.{ip_minor}.{i}.8' netmask='255.255.255.x'/>")
+                ip_minor += 1
+                if ip_minor == 256:
+                    ip_minor = 0
+                    ip_major += 1
+        interfaces.append(f"<interface hosts='**' address='255.x.x.x' netmask='255.255.255.x'/>")
+        return interfaces
 
     def create_ned_file(self):
         ned_str = ""
@@ -310,8 +438,21 @@ class Network:
         ini_str += "**.ppp[*].ppp.queue.packetCapacity = 100\n"
         ini_str += "\n"
 
+        ini_str += "# Configurator settings\n"
+        ini_str += "*.configurator.dumpAddresses = true\n"
+        ini_str += "*.configurator.dumpTopology = true\n"
+        ini_str += "*.configurator.dumpLinks = true\n"
+        ini_str += "*.configurator.dumpRoutes = true\n"
+        ini_str += f"*.configurator.config = xmldoc(\"{self._name}.xml\")\n"
+        ini_str += "\n"
+
+        if (self._visualizer):
+            ini_str += "# Visualizer settings\n"
+            ini_str += "*.visualizer.*.interfaceTableVisualizer.displayInterfaceTables = true\n"
+            ini_str += "\n"
+
         for name, node in self._node_map.items():
-            if node.get_type() == SERVER_TYPE:
+            if isinstance(node, Sink):
                 root = f"**.{name}"
                 ini_str += f"# Setup TCP server \"{node.get_name()}\"\n"
                 ini_str += f"{root}.numApps = 1\n"
@@ -322,7 +463,7 @@ class Network:
                 ini_str += "\n"
 
         for name, node in self._node_map.items():
-            if node.get_type() == CLIENT_TYPE:
+            if isinstance(node, Source):
                 ini_str += f"# Setup TCP client \"{node.get_name()}\"\n"
                 ini_str += node.get_flows()
                 if (node.get_num_flows() == 0):
@@ -335,7 +476,20 @@ class Network:
         fd.close()
 
         return ini_str
+    
+    def create_xml_file(self):
+        xml_str = ""
+        xml_str += "<config>\n"
+        for interface in self.xml_interfaces():
+            xml_str += f"{TAB}{interface}\n"
+        xml_str += "</config>\n"
+        fd = open(f"{WORKING_DIRECTORY}/{self._name}.xml", "w")
+        fd.write(xml_str)
+        fd.close()
 
+        return xml_str
+
+"""
 class FatTree(Network):
     def __init__(self, name, switch_degree, package=PACKAGE, imports=IMPORTS, width=800, height=500):
         super().__init__(name, package, imports, width, height)
@@ -358,9 +512,9 @@ class FatTree(Network):
         for i in range(self._k ** 2 // 4):
             self._node_map[f"core_{z(i)}"] = Node(f"core_{z(i)}", ROUTER_TYPE, (i + 0.5) * self._width / (self._k ** 2 / 4), 100)
         for i in range(self._k ** 3 // 4):
-            node_type = SERVER_TYPE if i % 4 == 3 else CLIENT_TYPE
+            node_type = SERVER_TYPE if i % 2 == 1 else CLIENT_TYPE
             self._node_map[f"host_{z(i)}"] = Node(f"host_{z(i)}", node_type, (i + 0.5) * self._width / (self._k ** 3 / 4), 400)
-            if i % 4 == 3:
+            if i % 2 == 1:
                 self._servers.append(self._node_map[f"host_{z(i)}"])
             else:
                 self._clients.append(self._node_map[f"host_{z(i)}"])
@@ -530,20 +684,29 @@ def generate_random_flows(clients, servers, n):
             while p < 3:
                 p = np.random.normal(4, 1)
             flows.append(10 ** p)
-    for i in range(n):
-        client = random.sample(clients, 1)[0]
-        server = random.sample(servers, 1)[0]
-        client.add_flow(server, flows[i], 0)
+    
+    for i, client in enumerate(clients):
+        server = servers[(i + len(servers) // 2) % len(servers)]
+        client.add_flow(server, 12500000, 0)  # 10 Mbit flow
+
+    #for i in range(n):
+    #    client = random.sample(clients, 1)[0]
+    #    server = random.sample(servers, 1)[0]
+    #    # client.add_flow(server, flows[i], 0)
+    #    client.add_flow(server, 1250000, 0)  # 1 Mbit flow
+"""
 
 def main():
     random.seed(0)
     np.random.seed(0)
 
-    network = Network(name="Default").initialize()
+    network = Network(name="Default", use_visualizer=True).initialize()
     ned_str = network.create_ned_file()
     ini_str = network.create_ini_file()
-    print(f"{ned_str}\n{ini_str}\n\n")
+    xml_str = network.create_xml_file()
+    print(f"{ned_str}\n{ini_str}\n{xml_str}\n")
 
+    """
     network = FatTree(name="FatTree", switch_degree=4).initialize()
     ned_str = network.create_ned_file()
     ini_str = network.create_ini_file()
@@ -558,7 +721,7 @@ def main():
     ned_str = network.create_ned_file()
     ini_str = network.create_ini_file()
     print(f"{ned_str}\n{ini_str}")
-
+    """
     return
 
 

20ae19a28eca68fa4a367113463e690275a0759a
Shravan Venkatesan <svenka16@jh.edu>
2022-04-15 19:06:13 -0400

    Simulation command relative paths

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ OMNET_PATH="/c/Users/shrav/Documents/_Docs_/JHU/Classes/omnetpp-5.7-windows-x86_
 # Root of OMNET++ working directory for project. Should have inet support. 
 OMNET_WORKINGDIR_PATH="/c/Users/shrav/Documents/_Docs_/JHU/Classes/omnetpp-5.7-windows-x86_64/omnetpp-5.7/samples/inet4/examples/inet/cc_datacenter";
 # Package name of OMNET++ working directory. 
-OMNET_WORKINGDIR_PACKAGE="inet.examples.inet.cc_datacenter;"
+OMNET_WORKINGDIR_PACKAGE="inet.examples.inet.cc_datacenter"
 # Python generation script for .ned, .ini, .xml files
 GENERATOR_PATH="/c/Users/shrav/Documents/_Docs_/JHU/Classes/cloud-computing-project/main.py";
 # Desired output directory
@@ -17,10 +17,11 @@ cd "${OMNET_PATH}/samples/inet4"
 make MODE=release -j8 all
 
 cd "${OMNET_WORKINGDIR_PATH}"
-cat "${OMNET_WORKINGDIR_PATH}/topologies.txt" | while read TOPO; do
+while read -r -a line; do
+    TOPO=${line[0]}
     echo "********** Running ${TOPO} simulation... **********"
-    ${OMNET_PATH}/bin/opp_run.exe -r 0 -m -u Cmdenv -n "${OMNET_PATH}/samples/inet4/src;${OMNET_PATH}/samples/inet4/tutorials;${OMNET_PATH}/samples/inet4/showcases" --image-path="${OMNET_PATH}/samples/inet4/images" -l "${OMNET_PATH}/samples/inet4/src/INET" "${OMNET_WORKINGDIR_PATH}/${TOPO}.ini" --output-scalar-file="${OUTPUT_PATH}/${TOPO}.sca" --output-vector-file="${OUTPUT_PATH}/${TOPO}.vec"
-done
+    ${OMNET_PATH}/bin/opp_run.exe -r 0 -m -u Cmdenv -n "../../;../../../src;../../../tutorials;../../../showcases" --image-path="$../../../images" -l "../../../src/INET" "${TOPO}.ini" --output-scalar-file="${OUTPUT_PATH}/${TOPO}.sca" --output-vector-file="${OUTPUT_PATH}/${TOPO}.vec"
+done < "${OMNET_WORKINGDIR_PATH}/topologies.txt"
 
 echo "********** Collecting results... **********"
 cd "${OUTPUT_PATH}"

8129cfbe440091c8b283ec718d609730b27fd207
bfremin1 <bfremin1@jhu.edu>
2022-04-24 14:17:32 -0400

    Added plotting mechanism

--- m_omnetpp_result_analyzer.py
+++ m_omnetpp_result_analyzer.py
@@ -12,6 +12,9 @@ from jellyfish_ned import Jellyfish
 from vl2_ned import VL2
 from flow_generator import FlowGenerator
 
+LINK_RATE = 1000
+FATTREE_4_LATENCY = 1.8755624188311693
+
 def main():
     show_plots = True
     if (len(sys.argv) > 2):
@@ -20,26 +23,48 @@ def main():
         set_working_directory(sys.argv[1])    
     if (len(sys.argv) > 2):
         show_plots = False
-    
     print(f"Working directory set to: \'{get_working_directory()}\'")
-    analysis_dir = f"{get_working_directory()}/analysis"
-    if not os.path.exists(analysis_dir):
-        os.makedirs(analysis_dir)
 
-    topologies, stats, runtimes, latencies = read_data()
-    print(topologies)
-    print(stats)
+    if not os.path.exists(f"{get_working_directory()}/analysis"):
+        print(f"ERROR: No analysis directory at: {get_working_directory()}/analysis")
+
+    for file in os.listdir(f"{get_working_directory()}/analysis"):
+        analysis_dir = f"{get_working_directory()}/analysis/{os.fsdecode(file)}"
+        analyze_topology_file(analysis_dir, show_plots=show_plots)
+
+def clean_key(key):
+    if "__" in key:
+        return key.split("__")[0]
+    else:
+        return key
+
+def weighted(arr, weights):
+    print(weights)
+    total_weight = sum(weights)
+    return [ a * w / total_weight for a, w in zip(arr, weights)]
+
+def analyze_topology_file(analysis_dir, show_plots=True):
+    topologies_filename = f"{analysis_dir}/topologies.txt"
+    print(f"Processing {topologies_filename} ... ", flush=True)
+    topologies, stats, runtimes, latencies = read_data(topologies_filename)
+    # print(topologies)
+    topo_throughputs = {}
+    topo_latencies = {}
+    topo_flow_size = {}
+    for topo in topologies:
+        topo_throughputs[clean_key(topo)] = []
+        topo_latencies[clean_key(topo)] = []
+        topo_flow_size[clean_key(topo)] = []
     for topo in topologies:
         fd = open(f"{get_working_directory()}/{topo}.pkl", "rb")
         network = pkl.load(fd)
         fd.close()
-        fd = open(f"{get_working_directory()}/{topo}_flowstats.txt", "w")
-        server_throughputs = {}
+        fd = open(f"{analysis_dir}/{topo}_flowstats.txt", "w")
+        server_total_throughput = {}
         server_latencies = {}
-        server_lengths = {}
-        server_flow_sizes = {}
-        xs = []
-        ys = []
+        #server_lengths = {}
+        #server_flow_sizes = {}
+        #server_runtimes = {}
         for node in network._node_map.values():
             if isinstance(node, Source):
                 server_name = node.get_server_name()
@@ -47,53 +72,83 @@ def main():
                     m_name = f"{topo}.{flow._src}.app[0]"
                     throughput_kb = flow._send_amount / runtimes[m_name] / 1000
                     fd.write(f"module={m_name} server={server_name} src={flow._src} dest={flow._dest} path_len={len(flow._path) - 3} throughput_kb={throughput_kb} latency_ms={latencies[m_name]} runtime={runtimes[m_name]} num_bytes={flow._send_amount}\n")  
-                    xs.append(flow._send_amount)
-                    ys.append(latencies[m_name])
-                    if server_name in server_throughputs.keys():
-                        server_throughputs[server_name] += throughput_kb
+                    topo_throughputs[clean_key(topo)].append(throughput_kb)
+                    topo_latencies[clean_key(topo)].append(latencies[m_name])
+                    topo_flow_size[clean_key(topo)].append(flow._send_amount)
+                    if server_name in server_total_throughput.keys():
+                        server_total_throughput[server_name] += throughput_kb
                         server_latencies[server_name].append(latencies[m_name])
-                        server_lengths[server_name].append(len(flow._path) - 3)
-                        server_flow_sizes[server_name].append(flow._send_amount)
+                        #server_lengths[server_name].append(len(flow._path) - 3)
+                        #server_flow_sizes[server_name].append(flow._send_amount)
+                        #server_runtimes[server_name].append(runtimes[m_name])
                     else:
-                        server_throughputs[server_name] = throughput_kb
+                        server_total_throughput[server_name] = throughput_kb
                         server_latencies[server_name] = [latencies[m_name]]
-                        server_lengths[server_name] = [len(flow._path) - 3]
-                        server_flow_sizes[server_name] = [flow._send_amount]
+                        #server_lengths[server_name] = [len(flow._path) - 3]
+                        #server_flow_sizes[server_name] = [flow._send_amount]
+                        #server_runtimes[server_name] = [runtimes[m_name]]
         fd.close()
-        plt.scatter(xs, ys, label=topo)
+        
+        for server_name, throughput_kb in server_total_throughput.items():
+            if throughput_kb > LINK_RATE:  # Omnet++ seems to handle give too large of throughputs to small flows
+                server_total_throughput[server_name] = LINK_RATE
         for key, value in stats.items():
             if key in topo:
-                print(key, topo)
-                value['x'].extend(list(server_throughputs.values()))
+                value['x'].extend(list(server_total_throughput.values()))
                 value['y'].extend([np.mean(lats) for lats in server_latencies.values()])
 
-    plt.xlabel('flow size (B)')
-    plt.ylabel('latency (ms)')
+    for topo in topo_throughputs.keys():
+        if 'jitter' in topologies[0]:
+            topo_flow_size[topo] = [ x + np.random.normal(0, 4000) for x in topo_flow_size[topo]]
+        topo_throughputs[topo] = [ x / LINK_RATE for x in topo_throughputs[topo] ]
+        topo_latencies[topo] = [ y / FATTREE_4_LATENCY for y in topo_latencies[topo] ]
+
+    for topo in topo_throughputs.keys():  
+        plt.scatter(topo_flow_size[topo], topo_latencies[topo], label=topo, s=10, alpha=0.5)
+    plt.xlabel('Flow Size (B)')
+    plt.ylabel('Normalized Latency (*)')
     plt.legend()
-    plt.savefig(f"{analysis_dir}/size_latency_plot.png")
+    plt.savefig(f"{analysis_dir}/latency_vs_size_plot.png")
+    if show_plots:
+        plt.show()
+    plt.close()
+
+    for topo in topo_throughputs.keys():  
+        plt.scatter(topo_flow_size[topo], topo_throughputs[topo], label=topo, s=10, alpha=0.5)
+    plt.xlabel('Flow Size (B)')
+    plt.ylabel('Normalized Throughput (*)')
+    plt.legend()
+    plt.savefig(f"{analysis_dir}/throughput_vs_size_plot.png")
     if show_plots:
         plt.show()
     plt.close()
 
     analysis_str = ""
     for key, value in stats.items():
-        plt.scatter(value['x'], value['y'], label=key)
-        plt.errorbar(np.mean(value['x']), np.mean(value['y']), xerr=np.std(value['x']), yerr=np.std(value['y']), capsize=4, alpha=0.5)
-        analysis_str += f"key={key}\n"
-        analysis_str += f"X Data: n={len(value['x'])} min={min(value['x'])} max={max(value['x'])} mean={np.mean(value['x'])} stdev={np.std(value['x'])}\n"
-        analysis_str+= f"Y Data: n={len(value['y'])} min={min(value['y'])} max={max(value['y'])} mean={np.mean(value['y'])} stdev={np.std(value['y'])}\n"
+        # print(value['w'])
+        xs = [ x / LINK_RATE for x in value['x']]
+        ys = [ y / FATTREE_4_LATENCY for y in value['y']]
+        prop_full_send = len([x for x in filter(lambda x: x == 1, xs)]) / len(xs)
+        plt.scatter(xs, ys, label=clean_key(key), alpha=0.5, s=10)
+        plt.errorbar(np.mean(xs), np.mean(ys), xerr=np.std(xs), yerr=np.std(ys), capsize=4)
+        analysis_str += f"key={key} n={len(xs)} link_rate={LINK_RATE} fattree_latency={FATTREE_4_LATENCY} prop_full_send={prop_full_send}\n"
+        analysis_str += f"Raw_X_Data: min={min(value['x'])} max={max(value['x'])} mean={np.mean(value['x'])} stdev={np.std(value['x'])}\n"
+        analysis_str += f"Raw_Y_Data: min={min(value['y'])} max={max(value['y'])} mean={np.mean(value['y'])} stdev={np.std(value['y'])}\n"
+        analysis_str += f"Normalized_X_Data: min={min(xs)} max={max(xs)} mean={np.mean(xs)} stdev={np.std(xs)}\n"
+        analysis_str += f"Normalized_Y_Data: min={min(ys)} max={max(ys)} mean={np.mean(ys)} stdev={np.std(ys)}\n"
     
     analysis_file = open(f"{analysis_dir}/analysis.txt", "w")
     analysis_file.write(analysis_str)
     analysis_file.close()
-    print(analysis_str)
+    # print(analysis_str)
 
-    plt.xlabel("Throughput (kB/sec)")
-    plt.ylabel("Latency (ms)")
+    plt.xlabel("Normalized Throughput (*)")
+    plt.ylabel("Normalized Latency (*)")
     plt.legend()
     plt.savefig(f"{analysis_dir}/throughput_latency_plot.png")
     if show_plots:
         plt.show()
+
 
 def get_cleaned_dfs(path):
     df = pd.read_csv(path)
@@ -114,8 +169,9 @@ def get_cleaned_dfs(path):
 
     return df_times, df_delays
 
-def read_data():    
-    fd = open(f"{get_working_directory()}/topologies.txt", "r")
+def read_data(topologies_filename):    
+    # fd = open(f"{get_working_directory()}/topologies.txt", "r")
+    fd = open(topologies_filename, "r")
     topologies = []
     stats = {}
     for line in fd.readlines():
